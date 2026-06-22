34e95c60d2081b4db4a82d97ce75ea3773703bd2
Ridheemadhouni <ridhimadhoni6@gmail.com>
2026-06-22 12:28:28 +0530

    Updated calculator.c

--- calculator.c
+++ calculator.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main() {
@@ -44,51 +43,4 @@ int main() {
     }
 
     return 0;
-=======
-#include <stdio.h>
-
-int main() {
-    int choice;
-    float num1, num2, result;
-
-    printf("Calculator Menu\n");
-    printf("1. Addition\n");
-    printf("2. Subtraction\n");
-    printf("3. Multiplication\n");
-    printf("4. Division\n");
-    printf("Enter your choice (1-4): ");
-    scanf("%d", &choice);
-
-    printf("Enter two numbers: ");
-    scanf("%f %f", &num1, &num2);
-
-    switch (choice) {
-        case 1:
-            result = num1 + num2;
-            printf("Result = %.2f", result);
-            break;
-
-        case 2:
-            result = num1 - num2;
-            printf("Result = %.2f", result);
-            break;
-
-        case 3:
-            result = num1 * num2;
-            printf("Result = %.2f", result);
-            break;
-
-        case 4:
-            if (num2 != 0)
-                printf("Result = %.2f", num1 / num2);
-            else
-                printf("Error! Division by zero.");
-            break;
-
-        default:
-            printf("Invalid Choice!");
-    }
-
-    return 0;
->>>>>>> 23650d715680b382697dcf84d52fcc89482c56e2
 }
